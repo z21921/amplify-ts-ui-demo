a8c2a1e67b43e400998b1c02bdc0072f380b04fa
z21921 <z21921@hotmail.com>
2025-02-03 10:01:49 +0800

    auth

--- src/App1.tsx
+++ src/App1.tsx
@@ -2,10 +2,11 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { useAuthenticator } from '@aws-amplify/ui-react';
 
 function App1() {
   const [count, setCount] = useState(0)
-
+  const { signOut } = useAuthenticator();
   return (
     <>
       <div>
@@ -24,6 +25,7 @@ function App1() {
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
+        <button onClick={signOut}>Sign out</button>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Authenticator } from '@aws-amplify/ui-react';
 import './index.css'
 import App from './App.tsx'
 import App1 from './App1.tsx'
+import '@aws-amplify/ui-react/styles.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-    <App1 />
+    <Authenticator>
+      <App />
+      <App1 />
+    </Authenticator>
   </StrictMode>,
 )
+
+
+  
+{/* <React.StrictMode>
+<Authenticator>
+  <App />
+</Authenticator>
+</React.StrictMode> */}
